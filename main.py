280a58ec92a38975157cb9b803222bdbebf0df17
EgorYarky <yastreb1239170@gmail.com>
2025-12-01 18:33:57 +1000

    add a cycle and, now it return a one list with coplimentary right DNA sequence

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import random
 coplimentary = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',}
 symbols = "ATCG"
 password_length = 8
-random_dna_base = random.choices(symbols, k=password_length)
-random.shuffle(random_dna_base)
-print(f"The Random DNA Base: {random_dna_base}")
-#dna_base = ['A', 'C', 'A', 'G', 'T', 'T',]
+app_on = 1
+while app_on > 0:
+    random_dna_base = random.choices(symbols, k=password_length)
+    random.shuffle(random_dna_base)
+    #print(f"The Random DNA Base: {random_dna_base}")
+    #dna_base = ['A', 'C', 'A', 'G', 'T', 'T',]
 
-def make_dna(base):
-    coplimentary_dna = []
-    for a in base:
-        coplimentary_dna.append(coplimentary[a])
+    def make_dna(base):
+        coplimentary_dna = []
+        count = 0
+        for a in base:
+            coplimentary_dna.append(random_dna_base[count])
+            coplimentary_dna.append(coplimentary[a])
+            count = count + 1
+            
+        return coplimentary_dna
         
-    return coplimentary_dna
-
-
-print(make_dna(random_dna_base))
+    print(f"Result: {make_dna(random_dna_base)}")
+    app_on = int(input("Enter 1 To Countinue, Or Enter 0: "))
