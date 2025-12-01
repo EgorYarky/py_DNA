c7a9b2f7bb9ebaf380f98502c4f6e48d6efc3527
EgorYarky <yastreb1239170@gmail.com>
2025-12-01 15:07:04 +1000

    I add, Random DNA Base Genrator, Print A What Generate, And Result Return Like A List

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import random
 coplimentary = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',}
-
-dna_base = ['A', 'C', 'A', 'G', 'T', 'T',]
+symbols = "ATCG"
+password_length = 8
+random_dna_base = random.choices(symbols, k=password_length)
+random.shuffle(random_dna_base)
+print(f"The Random DNA Base: {random_dna_base}")
+#dna_base = ['A', 'C', 'A', 'G', 'T', 'T',]
 
 def make_dna(base):
+    coplimentary_dna = []
     for a in base:
-        print(coplimentary[a])
-        print(a)
+        coplimentary_dna.append(coplimentary[a])
+        
+    return coplimentary_dna
 
-make_dna(dna_base)
+
+print(make_dna(random_dna_base))
